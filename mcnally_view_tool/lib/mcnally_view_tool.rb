0b41da223d9ff93f1da84f065dbdafd9884f76a4
George McNally <geomcn11@gmail.com>
2018-11-23 16:00:00 +0000

    yeah

---
+++ mcnally_view_tool/lib/mcnally_view_tool.rb
@@ -0,0 +1,6 @@
+require "mcnally_view_tool/version"
+require "mcnally_view_tool/renderer"
+module McnallyViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ mcnally_view_tool/lib/mcnally_view_tool/version.rb
@@ -0,0 +1,3 @@
+module McnallyViewTool
+  VERSION = "0.1.0"
+end
